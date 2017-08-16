383d33ed88eff59f0747ae33b353e890adc75d7c
bannieblue <l.jngrui@gmail.com>
2017-08-16 16:49:28 +0800

    change style

--- js/blog.js
+++ js/blog.js
@@ -2,19 +2,36 @@
  * Created by bannie on 2017/8/8.
  */
 $(function(){
-    $.get('es6.md', function(data) {
+    var defaultActive =$("#blog ul.blog li").find("li").eq(0);
+    defaultActive.addClass("active");
+
+    $.get(defaultActive.find("a").attr("name")+".md", function(data) {
         var converter = new showdown.Converter();
         var html = converter.makeHtml(data);
         $('#result').html(html);
     });
 
-    $(".middle-left ul").on("click","li",function(){
-        var name = $(this).attr("name")+".md";
+    $("#blog ul.blog li").on("click","li",function(){
+        $("#blog ul.blog ").find("li").removeClass("active");
+        $(this).addClass("active");
+        var name = $(this).find("a").attr("name")+".md";
         $.get(name, function(data) {
             var converter = new showdown.Converter();
             var html = converter.makeHtml(data);
             $('#result').html(html);
         });
+    });
+    $(".navbar ul.nav > li").on("click",function(){
+        $("ul.nav > li").removeClass("active");
+        $(this).addClass("active");
+        if(this.id == "cvNav"){
+            $("#blog").hide();
+            $("#cv").show();
+        }
+        if(this.id == "blogNav"){
+            $("#cv").hide();
+            $("#blog").show();
+        }
     });
 });
 
